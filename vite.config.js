c47d248bb9c44cdd786164969717ede33feeb43d
Mohimem <sulimangzllal@alMacBook-Pro-mohimen-tantoush.local>
2025-10-07 06:36:18 +0200

    new updates for release to clinic

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server:{
+        host: "172.16.101.100",
+        port:"5173",
+        strictPort: true,
+        cors: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
